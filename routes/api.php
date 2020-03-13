1163b807b8caab6cee0702bc27f4dceace634b8b
Fernando Bevilacqua <dovyski@gmail.com>
2020-03-13 08:05:39 -0300

    Adiciona controllers mínimos para autenticação oauth

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::post('login', 'AuthController@login');
+Route::post('register', 'AuthController@register');
+ 
+Route::middleware('auth:api')->group(function () {
+    Route::get('user', 'AuthController@details');
+    Route::resource('products', 'ProductController');
 });
