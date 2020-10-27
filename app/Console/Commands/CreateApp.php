ddbb7fda8f9be84d16ef6e4d324fcc4dad994eb3
Fernando Bevilacqua <dovyski@gmail.com>
2020-10-27 13:43:08 -0300

    Adiciona comando para criar apps

---
+++ app/Console/Commands/CreateApp.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
+
+class CreateApp extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:create';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Cria uma nova aplicação';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $this->info('Criação de nova aplicação');
+        
+        $name = $this->ask('Nome da aplicação');
+        $email = $this->ask('E-mail da aplicação');
+        $password = $this->secret('Senha da aplicação');
+
+        if (!$this->confirm('Confirma criação?')) {
+            return 1;
+        }
+
+        $user = new User([
+            'name' => $name,
+            'email' => $email,
+            'password' => Hash::make($password)
+        ]);
+        
+        $user->save();
+        $this->info('Aplicação criada com sucesso!');
+
+        return 0;
+    }
+}
