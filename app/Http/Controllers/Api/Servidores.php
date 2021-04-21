bd095405daca63c81779759a0260506bc56b22ed
Fernando Bevilacqua <dovyski@gmail.com>
2021-04-21 16:12:12 -0300

    feat(servidores) busca de servidores por nome e iduffs

--- app/Http/Controllers/Api/Servidores.php
+++ app/Http/Controllers/Api/Servidores.php
@@ -4,17 +4,57 @@ namespace App\Http\Controllers\Api;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\DB;
+use App\Utils\Sanitizer;
 
 class Servidores extends Controller
 {
     /**
-     * Lista dos os alunos de um determinado curso.
+     * 
      *
+     * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
     public function busca(Request $request)
     {
-        $alunos = [];
-        return response()->json($alunos, 200, [], JSON_NUMERIC_CHECK);
-    }
+        $nome = $request->get('nome', '');
+        $iduffs = $request->get('iduffs', '');
+        $limit = $request->get('limit', 10);
+
+        $query = DB::connection('pessoas')->table('personnel')->limit($limit);
+
+        if (!empty($nome)) {
+            $query->whereRaw('name LIKE ?', ['%' . Sanitizer::clean($nome) .'%']);
+        }
+
+        if (!empty($iduffs)) {
+            $query->whereRaw('uid LIKE ?', ['%' . Sanitizer::clean($iduffs) .'%']);
+        }        
+        
+        $personnel = $query->get();
+        $result = [];
+
+        foreach($personnel as $person) {
+            $name = Sanitizer::clean($person->name);
+
+            $item = new \stdClass();
+            $item->nome = ucwords($name);
+            $item->iduffs = $person->uid;
+            $item->email = $person->email;
+            $item->telefone = $person->phone;
+            $item->voip = $person->voip;
+            $item->departamento = (object) [
+                'nome' => $person->department_name,
+                'iniciais' => $person->department_initials,
+                'endereco' => $person->department_address,
+            ];
+            $item->cargo = $person->job;
+            $item->notas = $person->notes;
+
+            $result[$person->uid] = $item;
+        }
+
+        $values = array_values($result);
+        return response()->json($values, 200, [], JSON_NUMERIC_CHECK);
+    }    
 }
--- config/database.php
+++ config/database.php
@@ -51,6 +51,14 @@ return [
             'foreign_key_constraints' => env('DB_FOREIGN_KEYS', true),
         ],
 
+        'pessoas' => [
+            'driver' => 'sqlite',
+            'url' => env('DATABASE_URL'),
+            'database' => env('DB_DATABASE_UFFS_PERSONEL', database_path('external/uffs-personnel/info/uffs-personnel.sqlite')),
+            'prefix' => '',
+            'foreign_key_constraints' => env('DB_FOREIGN_KEYS', true),
+        ],        
+
         'mysql' => [
             'driver' => 'mysql',
             'url' => env('DATABASE_URL'),
