4b71d4d1b9878c4f0e628c387578de6c6476af6f
Fernando Bevilacqua <dovyski@gmail.com>
2021-04-30 08:36:41 -0300

    feat(alunos) endpoint para listagem de alunos de um curso

--- app/Http/Controllers/Api/Curso.php
+++ app/Http/Controllers/Api/Curso.php
@@ -4,6 +4,9 @@ namespace App\Http\Controllers\Api;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Models\Scraper;
+use App\Support\Facades\SgaScraper;
+use Illuminate\Support\Facades\Cache;
 
 class Curso extends Controller
 {
@@ -14,7 +17,24 @@ class Curso extends Controller
      */
     public function alunos(Request $request, $iduffs)
     {
-        $alunos = [];
-        return response()->json($alunos, 200, [], JSON_NUMERIC_CHECK);
+        $scraper = Scraper::where('actor', $iduffs)->first();
+
+        if(!$scraper) {
+            return response()->json(['error' => "O curso identificado por '$iduffs' não foi encontrado."], 500);
+        }
+
+        $credenciais = [
+            'usuario' => $scraper->access_user,
+            'senha' => $scraper->access_password
+        ];
+
+        $cacheKey = 'alunos:' . $iduffs;
+        $cacheTtl = now()->addDays(2);
+
+        $info = Cache::remember($cacheKey, $cacheTtl, function() use ($credenciais) {
+            return SgaScraper::usando($credenciais)->alunos()->get();
+        });
+
+        return response()->json($info, 200, [], JSON_NUMERIC_CHECK);
     }
 }
